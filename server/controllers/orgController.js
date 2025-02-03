6a3b6af84a45217eaee186c2b9a9bb309d3fb384
dawit2123 <dawitzewdu2123@gmail.com>
2025-02-04 00:13:36 +0300

    code refactored

--- server/controllers/orgController.js
+++ server/controllers/orgController.js
@@ -4,7 +4,6 @@ const catchAsync = require("../utils/catchAsync");
 exports.createPosts = catchAsync(async (req, res, next) => {
   req.body.members = [];
   req.body.members.push(req.user.id);
-  console.log("req.body", req.body);
   const organization = await Organization.create(req.body);
   res.status(201).json({
     status: "success",
