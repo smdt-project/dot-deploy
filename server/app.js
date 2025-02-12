2f09007f53157cc9398fb3b4278cfa9e9ab9efc8
dawit2123 <dawitzewdu2123@gmail.com>
2025-02-12 20:56:21 +0300

    cors updated localhost

--- server/app.js
+++ server/app.js
@@ -23,6 +23,7 @@ app.use(
       "http://localhost:5173",
       "http://127.0.0.1:5173",
       "http://localhost:5174",
+      "http://127.0.0.1:5174",
       "https://dot-code-nu.vercel.app",
     ],
     credentials: true,
