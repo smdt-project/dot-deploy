264e8bd2ff286ef560ff17079a7011679eece03d
dawit2123 <dawitzewdu2123@gmail.com>
2025-02-12 20:54:24 +0300

    cors configured

--- server/app.js
+++ server/app.js
@@ -22,6 +22,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://127.0.0.1:5173",
+      "http://localhost:5174",
       "https://dot-code-nu.vercel.app",
     ],
     credentials: true,
